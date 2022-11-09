c07c49e8a28d2e6789a573f9497c227af1485c6f
Vanesflat <ptptcptptc@mail.ru>
2022-11-09 13:23:31 +0700

    Finished

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,6 @@
-$(function () {
+const headerNav = document.querySelector('.nav');
+const headerBtn = document.querySelector('.header__btn');
 
-})
+headerBtn.addEventListener('click', () => {
+  headerNav.classList.toggle('header__menu--open');
+});
